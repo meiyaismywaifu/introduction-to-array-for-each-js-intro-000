858bb376ee665235074238a330aaa8d642163143
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-12 23:51:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your doToElementsInArray() function here:
 function doToElementsInArray(array, callFunction){
-  array.forEach(callFunction);
+  array.forEach(callFunction); // this is not very illustrative.
 }
 
 // Add your changeCompletely() function here:

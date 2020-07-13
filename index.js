910d018e1c1f2f557380c10a04c3cbf686345f54
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-13 00:06:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Add your doToElementsInArray() function here:
-
+function doToElementsInArray(array, callFunction){
+  array.forEach(callFunction); // this is not very illustrative.
+}
 
 // Add your changeCompletely() function here:
+function changeCompletely(element, index, array){ // these are givens in array.forEach.
+  array[index] = 0; // all work consisted of visualizing what's being passed / exists where.
+}

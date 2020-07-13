a730b206a6c527d6ef243b7ba18b726358e55fea
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-13 00:03:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function doToElementsInArray(array, callFunction){
 }
 
 // Add your changeCompletely() function here:
-function changeCompletely(array){
+function changeCompletely(element, index, array){ // these exist for .forEach.
   console.log("test");
 }
